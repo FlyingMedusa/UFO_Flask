f20078c5e3662532f60ad621bb7826a06c765fcb
Marta Sleboda <sleboda.m98@gmail.com>
2021-07-21 19:55:23 +0200

    basics

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello! This is the main page"
+
+@app.route("/<name>")
+def user(name):
+    return f"Hello {name}"
+
+@app.route("/admin")
+def admin():
+    return redirect(url_for("user", name="Martha"))
+
+if __name__ == "__main__":
+    app.run()
