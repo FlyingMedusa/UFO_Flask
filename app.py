377ec4edb72049a8e475c774440a72776ad16403
Marta Sleboda <sleboda.m98@gmail.com>
2021-07-24 18:09:42 +0200

    subpages

--- app.py
+++ app.py
@@ -6,13 +6,29 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/<name>")
-def user(name):
-    return f"Hello {name}"
+@app.route("/historia")
+def history():
+    return render_template("historia.html")
 
-@app.route("/admin")
-def admin():
-    return redirect(url_for("user", name="Martha"))
+@app.route("/dzien1")
+def day1():
+    return render_template("dzien1.html")
+
+@app.route("/dzien2")
+def day2():
+    return render_template("dzien2.html")
+
+@app.route("/dzien3")
+def day3():
+    return render_template("dzien3.html")
+
+@app.route("/materialy")
+def materials():
+    return render_template("materialy.html")
+
+@app.route("/dni")
+def all_days():
+    return render_template("dni.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
